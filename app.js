32885782ede9090423da74bf300594fe1d4eda35
Alistair <alistairgrant95@gmail.com>
2023-08-05 12:12:18 +0100

    added messages

--- app.js
+++ app.js
@@ -49,6 +49,17 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+       const div = document.createElement('div');
+       div.className = `alert alert-${className}`;
+       div.appendChild(document.createTextNode(message));
+       const container = document.querySelector('.container');
+       const form = document.querySelector('#book-form');
+       container.insertBefore(div, form);
+       // Vanish in 3 secs
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -71,18 +82,28 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    // Instantiate book
-    const book = new Book(title, author, isbn);
+    // Validate
+    if(title === ''|| author === '' || isbn === '' ) {
+        UI.showAlert('Please fill in all fields', 'danger');
 
-    // Add Book to UI
-    UI.addBookToList(book);
+    } else {
+        // Instantiate book
+        const book = new Book(title, author, isbn);
 
-    // Clear fields
-    UI.clearFields();
+        // Add Book to UI
+        UI.addBookToList(book);
 
+        // Show message
+        UI.showAlert('Book Added', 'success');
+
+        // Clear fields
+        UI.clearFields();
+    }
 });
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target);
+
+    UI.showAlert('Book Removed', 'success');
 });
